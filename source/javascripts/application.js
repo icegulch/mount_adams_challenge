acf96d44b61cbe63498dd6d55f702e4460ac3ae7
icegulch <icegulch@gmail.com>
2015-01-03 20:54:25 -0800

    redesign checkpoint

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -10,14 +10,16 @@
     //   var kids = jQuery(this).find('.fade');
     //   kids.toggleClass('in');
     // }, { offset: 1 });
-    jQuery('#welcome .fade').addClass('in');
+    // jQuery('#welcome .fade').addClass('in');
+    jQuery('.cta-button').addClass('bounce');
 
     // var waypointOffset = jQuery('[data-waypoint-offset]');
     // var pullTrigger = jQuery('[data-waypoint-trigger]');
     // var damnKids = jQuery('[data-waypoint-trigger]');
-    // jQuery(pullTrigger).waypoint(function() {
-    //   jQuery(damnKids).toggleClass('in');
-    // }, { offset: waypointOffset });
+    jQuery('.slide-left').waypoint(function() {
+
+      jQuery(this).toggleClass('in');
+      }, { offset: '70%' });
 
   });
 
